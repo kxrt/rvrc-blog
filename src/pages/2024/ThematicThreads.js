449200a55b23f2d529c89082185a6da19f5f2966
jasonqiu212 <jason_qiu@hotmail.com>
2024-02-22 19:51:03 +0800

    2024: Add Thematic Threads page

---
+++ src/pages/2024/ThematicThreads.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+import Thread2 from "../../components/2024/thematic-threads/Thread2";
+import Thread3 from "../../components/2024/thematic-threads/Thread3";
+
+const Adventure = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          bgcolor: "#592693",
+          padding: { xs: "3%", md: "1%" },
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: "white",
+            fontSize: { xs: "24pt", md: "40pt" },
+            fontFamily: "Jost",
+            fontWeight: "Bold",
+          }}
+        >
+          Thematic Threads
+        </Typography>
+      </Box>
+      <Thread2 />
+      <Thread3 />
+    </>
+  );
+};
+
+export default Adventure;

9c97f7d229d878d387374198a7bf3b8eff4c226f
jasonqiu212 <jason_qiu@hotmail.com>
2024-03-01 22:33:51 +0800

    Abstract component for reuse

--- src/pages/2022/Landing.js
+++ src/pages/2022/Landing.js
@@ -9,6 +9,40 @@ import AwardWinners from "../../components/2022/landing/AwardWinners";
 // import PastSymposia from "../components/LandingComponents/PastSymposia";
 import StudentPoints from "../../components/2022/landing/StudentPoints";
 
+// import event1 from "../assets/2022/event1.png";
+import event2 from "../../assets/2022/event2.png";
+import event3 from "../../assets/2022/event3.png";
+import event4 from "../../assets/2022/event4.png";
+import event5 from "../../assets/2022/event5.png";
+import event6 from "../../assets/2022/event6.png";
+
+const images = [
+  // {
+  //   label: "event1",
+  //   imgPath: event1,
+  // },
+  {
+    label: "event2",
+    imgPath: event2,
+  },
+  {
+    label: "event3",
+    imgPath: event3,
+  },
+  {
+    label: "event4",
+    imgPath: event4,
+  },
+  {
+    label: "event5",
+    imgPath: event5,
+  },
+  {
+    label: "event6",
+    imgPath: event6,
+  },
+];
+
 const Landing = () => {
   return (
     <>
@@ -31,7 +65,7 @@ const Landing = () => {
           RVRC Symposium 2022
         </Typography>
       </Box>
-      <RVRCStepper />
+      <RVRCStepper images={images} />
       <StudentPoints />
       <p style={{ fontSize: "16pt", paddingInline: "10%" }}>
         Project abstracts can be viewed{" "}

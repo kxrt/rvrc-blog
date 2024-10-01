94e0f3ad5ecbbfce9f1b5e85423bb5dc294f20e1
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-01 10:54:56 +0800

    feat: duplicate pages for 2025 site

---
+++ src/pages/2025/KeynoteSpeaker.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import keynoteSpeaker from "../../assets/2024/keynote-speaker.png";
+
+const KeynoteSpeaker = () => {
+  return (
+    <>
+      <Box sx={{ bgcolor: "#592693", padding: { xs: "5%", md: "1.5%" } }}>
+        <Typography
+          variant="h1"
+          sx={{
+            color: "white",
+            fontSize: { xs: "24pt", md: "40pt" },
+            fontFamily: "Jost",
+          }}
+        >
+          Keynote Speaker
+        </Typography>
+      </Box>
+      <Box sx={{ display: { xs: "inline-block" } }}>
+        <Box sx={{ display: "inline-block" }}>
+          <img
+            src={keynoteSpeaker}
+            alt="Keynote Speaker"
+            width={250}
+            style={{ marginTop: "40px" }}
+          />
+        </Box>
+        <Box sx={{ textAlign: { xs: "center" } }}>
+          <Typography
+            sx={{
+              fontSize: { xs: "18pt", md: "24pt" },
+              paddingInline: "7%",
+              paddingTop: "18px",
+              fontFamily: "Jost",
+              color: "#1d9077",
+            }}
+          >
+            Mr. Victor Zhu
+          </Typography>
+          <Typography
+            style={{
+              fontSize: { xs: "12pt", md: "16pt" },
+              paddingInline: "7%",
+              paddingBottom: "0px",
+              fontFamily: "Jost",
+            }}
+          >
+            Founder and CEO, Hatch
+          </Typography>
+
+          <div>
+            <p
+              style={{
+                fontSize: "14pt",
+                paddingInline: "7%",
+                textAlign: "justify",
+              }}
+            >
+              RVRC Alumnus <b>Mr. Victor Zhu</b> is the Founder and CEO of{" "}
+              <b>
+                <a
+                  href="https://www.hatch.sg/"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ color: "#592693" }}
+                >
+                  Hatch
+                </a>
+              </b>
+              , an impact-driven business with the mission to leverage digital
+              as a tool for intentional impact. The business comprises two arms
+              - Hatch Academy, which runs skills and employability programs in
+              Digital Marketing and User Interface, User Experience (UI/UX)
+              Design for learners from all walks of life, as well as Hatch
+              Mediahouse, which provides a suite of digital and design services
+              to help small and medium-sized businesses and non-profits
+              digitalise. In his work, he brings in partnerships and resources
+              to make the dreams of social innovation a reality, such as
+              creating Singapore’s first digital employability program for
+              out-of-school and at-risk youths, and more recently, for persons
+              with disabilities.
+            </p>
+          </div>
+          <div>
+            {/* <p
+              style={{
+                fontSize: "14pt",
+                paddingInline: "7%",
+                textAlign: "justify",
+              }}
+            >
+              Mr. Swaminathan was an ideal choice for the opening keynote, given
+              the symposium theme. As an undergraduate at the NUS Faculty of
+              Engineering and the University Scholar’s Programme, he pivoted
+              away from a strictly academic pursuit to explore and acquire an
+              education that granted him real experience, exposure, and the room
+              to be creative. Aligned with RVRC’s{" "}
+              <i>‘World is our classroom’</i> approach to learning and tackling
+              real world problems, as a student he too worked on meaningful
+              projects that directly addressed sustainability and social issues
+              in Singapore and the SEA region. Hence, it was envisioned that he
+              would be able to inspire students with relevant and relatable
+              ideas on how they too could explore and personalise their
+              individual university education.
+            </p> */}
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default KeynoteSpeaker;
---
+++ src/pages/2025/Landing.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
+import RVRCStepper from "../../components/SwipeableTextMobileStepper";
+// import SignupButton from "../../components/2024/landing/SignupButton";
+import SymposiumTheme from "../../components/2024/landing/SymposiumTheme";
+import ProgrammeHighlights from "../../components/2024/landing/ProgrammeHighlights";
+import PastSymposia from "../../components/PastSymposia";
+import StudentPoints from "../../components/2024/landing/StudentPoints";
+
+import LandingBanner1 from "../../assets/2024/landing-banner-1.jpg";
+import LandingBanner2 from "../../assets/2024/landing-banner-2.jpg";
+import LandingBanner3 from "../../assets/2024/landing-banner-3.jpg";
+import LandingBanner4 from "../../assets/2024/landing-banner-4.jpg";
+import LandingBanner5 from "../../assets/2024/landing-banner-5.jpg";
+
+const images = [
+  {
+    label: "Guest of Honour",
+    imgPath: LandingBanner1,
+  },
+  {
+    label: "Participants",
+    imgPath: LandingBanner2,
+  },
+  {
+    label: "College Master",
+    imgPath: LandingBanner3,
+  },
+  {
+    label: "Lunch",
+    imgPath: LandingBanner4,
+  },
+  {
+    label: "Keynote Speaker",
+    imgPath: LandingBanner5,
+  },
+];
+
+const Landing = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          bgcolor: "#592693",
+          padding: { xs: "3%", md: "1%" },
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: "white",
+            fontSize: { xs: "24pt", md: "40pt" },
+            fontFamily: "Jost",
+            fontWeight: "Bold",
+          }}
+        >
+          RVRC Symposium 2025
+        </Typography>
+      </Box>
+
+      <RVRCStepper images={images} />
+
+      <StudentPoints />
+
+      <p style={{ fontSize: "16pt", paddingInline: "10%" }}>
+        We are honoured to have{" "}
+        <Link to="/keynote" style={{ color: "#592693" }}>
+          Dr. Adrian Loo
+        </Link>{" "}
+        as our Keynote Speaker. We believe that his unique story as an innovator
+        of solutions for environmental, economic and social sustainability
+        issues serve as a powerful inspiration for RVRC students.
+      </p>
+
+      <SymposiumTheme />
+      <ProgrammeHighlights />
+
+      {/* TODO: Enable when sign up starts */}
+      {/* <SignupButton /> */}
+
+      <PastSymposia />
+    </>
+  );
+};
+
+export default Landing;

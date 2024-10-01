50b3832e6446a74410d39cf019462befcb5372ff
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-01 10:54:36 +0800

    refactor: move past links

--- src/components/PastSymposia.js
+++ src/components/PastSymposia.js
@@ -1,7 +1,44 @@
 import React from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 
-import links from "./PastLinks";
+const links = [
+  // {
+  //   title: 'AY 2015/16',
+  //   href: 'http://blog.nus.edu.sg/gem1917/',
+  // },
+  // {
+  //   title: 'AY 2016/17',
+  //   href: 'https://blog.nus.edu.sg/geq1917/',
+  // },
+  // {
+  //   title: 'AY 2017/18',
+  //   href: 'https://blog.nus.edu.sg/geq2017/',
+  // },
+  {
+    title: "AY 2018/19",
+    href: "https://blog.nus.edu.sg/geq1917student2019/action-for-sustainability-symposium/",
+  },
+  {
+    title: "AY 2019/20",
+    href: "https://blog.nus.edu.sg/geq1917symposium/",
+  },
+  // {
+  //   title: 'AY 2020/21',
+  //   href: 'http://blog.nus.edu.sg/gem1917/',
+  // },
+  // {
+  //   title: 'AY 2021/22',
+  //   href: 'http://blog.nus.edu.sg/gem1917/',
+  // },
+  {
+    title: "AY 2022/23",
+    href: "http://rvrc-blog.vercel.app/2022",
+  },
+  {
+    title: "AY 2023/24",
+    href: "http://rvrc-blog.vercel.app/2024",
+  },
+];
 
 const PastSymposia = () => {
   return (

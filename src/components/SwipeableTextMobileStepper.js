5de93c18ae77b25171a0227dd8e15a8d484dc6e0
jasonqiu212 <jason_qiu@hotmail.com>
2025-05-09 21:36:51 +0800

    refactor: use swiper

--- src/components/SwipeableTextMobileStepper.js
+++ src/components/SwipeableTextMobileStepper.js
@@ -1,45 +1,32 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
 import { Box } from "@mui/material";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
 
 function SwipeableTextMobileStepper({ images }) {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
   return (
     images && (
       <Box sx={{ flexGrow: 1 }}>
-        <AutoPlaySwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={activeStep}
-          onChangeIndex={handleStepChange}
-          enableMouseEvents
+        <Swiper
+          modules={[Autoplay]}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop
+          slidesPerView={1}
         >
-          {images.map((step, index) => (
-            <div key={step.label}>
-              {Math.abs(activeStep - index) <= 2 ? (
-                <Box
-                  component="img"
-                  sx={{
-                    display: "block",
-                    overflow: "hidden",
-                    width: "100%",
-                  }}
-                  src={step.imgPath}
-                  alt={step.label}
-                />
-              ) : null}
-            </div>
+          {images.map((step) => (
+            <SwiperSlide key={step.label}>
+              <Box
+                component="img"
+                sx={{ display: "block", width: "100%" }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            </SwiperSlide>
           ))}
-        </AutoPlaySwipeableViews>
+        </Swiper>
       </Box>
     )
   );

4c7bfa125382b420d0fcd70b07310ba6bc146817
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-12-23 18:48:37 +0800

    feat(2026): add poster gallery information

---
+++ src/constants/2026/Posters.js
@@ -0,0 +1,112 @@
+export const thread1Posters = [
+  {
+    title: "Film Critique of Pride and Prejudice",
+    course: "RVX1003",
+    presenters: [
+      "Lee Jing Min",
+    ],
+    abstract: "",
+  },
+  {
+    title: "Reimagining the NUS Campus: Identifying Place Narratives — Alice Lee Plaza",
+    course: "RVSS1000",
+    presenters: [
+      "Neo Siao Shiuan",
+      "Quek Syn Hui",
+      "Shi Xinrui",
+    ],
+    abstract: "",
+  },
+  {
+    title: "Liveable Spaces and Connecting Communities at NUS",
+    course: "RVSS1001",
+    presenters: [
+      "Wayne Lee Jun Xian",
+      "Wang Xiaoqi",
+      "Wu Xinyue",
+      "Zheng Shun Ren",
+    ],
+    abstract: "",
+  },
+  {
+    title: "Food as Culture: Cosmopolitan Edition",
+    course: "RVSS1002",
+    presenters: [
+      "Ryan Toh",
+      "Yaohui",
+    ],
+    abstract: "",
+  },
+  {
+    title: "POV: A Singaporean Working in Spain",
+    course: "RVC1000",
+    presenters: [
+      "Lee Yern-Yu",
+      "Ivis Tan Leyi",
+      "Xu Huanghao",
+      "Wang Yifei",
+    ],
+    abstract: "",
+  },
+  {
+    title: "Decent Work and Economic Growth in Singapore",
+    course: "RVC1001",
+    presenters: [
+      "Sophia Rae Teo",
+      "Ernest Koh Guan Yu",
+      "Manu Dagur",
+    ],
+    abstract: "",
+  },
+  {
+    title: "How Culture Shapes Youth Involvement in Sustainability",
+    course: "RVC2000",
+    presenters: [
+      "Koor Jaye",
+      "Guan Yongzhi",
+      "J Aditya",
+      "Kuah Sze Min Shermaine",
+    ],
+    abstract: "",
+  },
+  {
+    title: "Dyslexia Unravelled",
+    course: "RVN2003",
+    presenters: [
+      "Adrian Lee Ze Kai",
+      "Goh Peng Kerng",
+      "Lai Woh Jon",
+      "Liew Kangze",
+      "Lim Yi Kai",
+    ],
+    abstract: "",
+  },
+];
+
+export const thread2Posters = [
+  {
+    title: "Climate Cafe",
+    presenters: ["Lee Shi Yi"],
+    abstract: "",
+  },
+  {
+    title: "RV CIP",
+    presenters: ["Hong Yi", "Jun Kang"],
+    abstract: "",
+  },
+  {
+    title: "PSS",
+    presenters: ["Hao Cheng"],
+    abstract: "",
+  },
+  {
+    title: "RV Explorers",
+    presenters: ["Najwa"],
+    abstract: "",
+  },
+  {
+    title: "Artelier",
+    presenters: ["Faiz"],
+    abstract: "",
+  },
+];
--- src/pages/2026/PosterGallery.js
+++ src/pages/2026/PosterGallery.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Footer from "../../components/Footer";
 import Title from "../../components/Title";
-import PosterGalleryImage from "../../assets/2025/poster-gallery.jpeg";
+// import PosterGalleryImage from "../../assets/2025/poster-gallery.jpeg";
 import ProgrammeHighlights from "../../components/highlights/ProgrammeHighlights";
 import ProjectCard from "../../components/ProjectCard";
-import { posterGalleryEvents } from "../../constants/2025/ProgrammeEvents";
-import { thread1Posters, thread2Posters } from "../../constants/2025/Speakers";
+import { posterGalleryEvents } from "../../constants/2026/ProgrammeEvents";
+import { thread1Posters, thread2Posters } from "../../constants/2026/Posters";
 
-const Thread1 = () => {
+const PosterGallery = () => {
   return (
     <Stack spacing={0}>
       <Title title="Poster Gallery" />
-      <Box component="img" src={PosterGalleryImage} sx={{ width: "100%" }} />
+      {/* <Box component="img" src={PosterGalleryImage} sx={{ width: "100%" }} /> */}
       <Stack
         spacing={4}
         sx={{
@@ -22,16 +22,19 @@ const Thread1 = () => {
         }}
       >
         <Typography sx={{ fontSize: { md: "16pt" }, color: "white" }}>
-          Featuring posters from Thread 1 and 2, the Poster Gallery highlighted
-          both academic learning and student-led initiatives. Attendees could
-          explore projects at their own pace, interact with the material, and
-          engage directly with creators encouraging deeper understanding and
-          personalized insights into each initiative. Attendees could also visit
-          Interactive Booths hosted by our community partners, NTUC, Hatch, the
-          moonbeam co., to learn about how they tackle complex
-          socio-environmental issues. These two parallel sessions was conducted
-          within a shared space for the tea break, enhancing meaningful learning
-          and networking opportunities for all.
+          Featuring posters from Threads 1 and 2, the Poster Gallery showcases both 
+          academic learning and student-led initiatives. Attendees are able to explore 
+          the projects at their own pace, engage with the materials and interact directly 
+          with the presenters, fostering deeper understanding and personalised insights 
+          into each initiative.
+        </Typography>
+        <Typography sx={{ fontSize: { md: "16pt" }, color: "white" }}>
+          Alongside the Poster Gallery, Interactive Booths hosted by our community partners, 
+          WWF, MINDS, and NTUC Youth, provide attendees with opportunities to learn about 
+          their work in advancing environmental conservation, social inclusion and youth 
+          advocacy. These parallel sessions are held within a shared space during the tea break, 
+          creating a vibrant environment that encouraged meaningful learning, dialogue and 
+          networking among participants.
         </Typography>
       </Stack>
       <ProgrammeHighlights
@@ -52,9 +55,25 @@ const Thread1 = () => {
           direction={{ xs: "column", lg: "row" }}
           spacing={2}
           justifyContent="center"
+          sx={{ paddingInline: "10%" }}
+        >
+          {thread1Posters.slice(0, 4).map((project) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              subtitle={project.course}
+              presenters={project.presenters}
+              abstract={project.abstract}
+            />
+          ))}
+        </Stack>
+        <Stack
+          direction={{ xs: "column", lg: "row" }}
+          spacing={2}
+          justifyContent="center"
           sx={{ paddingInline: "10%", marginBottom: "24px" }}
         >
-          {thread1Posters.map((project) => (
+          {thread1Posters.slice(4, 8).map((project) => (
             <ProjectCard
               key={project.title}
               title={project.title}
@@ -79,14 +98,14 @@ const Thread1 = () => {
           direction={{ xs: "column", lg: "row" }}
           spacing={2}
           justifyContent="center"
-          sx={{ paddingInline: "10%", marginBottom: "24px" }}
+          sx={{ paddingInline: "10%" }}
         >
           {thread2Posters.slice(0, 3).map((project) => (
             <ProjectCard
               key={project.title}
               title={project.title}
               presenters={project.presenters}
-              abstract={project.description}
+              abstract={project.abstract}
             />
           ))}
         </Stack>
@@ -101,7 +120,7 @@ const Thread1 = () => {
               key={project.title}
               title={project.title}
               presenters={project.presenters}
-              abstract={project.description}
+              abstract={project.abstract}
             />
           ))}
         </Stack>
@@ -112,4 +131,4 @@ const Thread1 = () => {
   );
 };
 
-export default Thread1;
+export default PosterGallery;
